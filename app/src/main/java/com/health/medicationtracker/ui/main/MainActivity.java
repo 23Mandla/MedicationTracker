34616150d009f9f4fe388bbd7c5a84e23e48116c
Mandla Mbolekwa <mandlambolekwa@gmail.com>
2025-08-23 14:38:48 +0200

    Updated user interface

--- app/src/main/java/com/health/medicationtracker/ui/main/MainActivity.java
+++ app/src/main/java/com/health/medicationtracker/ui/main/MainActivity.java
@@ -1,9 +1,15 @@
 package com.health.medicationtracker.ui.main;
 
+import android.graphics.drawable.ColorDrawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -20,6 +26,7 @@ import com.health.medicationtracker.ui.adapter.MedicationAdapter;
 import com.health.medicationtracker.viewmodel.MedicationViewModel;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
     //data source
@@ -42,6 +49,15 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        //TODO perhaps target icons depending on how the theme look
+        Window window = getWindow();
+        // Clear FLAG_TRANSLUCENT_STATUS flag:
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        // Add FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS flag to the window:
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        // Finally change the color:
+        window.setStatusBarColor(ContextCompat.getColor(this, R.color.navy));
 
         //data binding
         binding_main_activity = DataBindingUtil.setContentView(this, R.layout.activity_main);
