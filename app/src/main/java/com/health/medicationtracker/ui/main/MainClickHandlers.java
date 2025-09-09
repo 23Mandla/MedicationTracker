87660d8e50a897be58baa1de63219fc88a7f631f
Mandla Mbolekwa <mambolejhb024@student.wethinkcode.co.za>
2025-09-09 05:50:21 +0200

    Added method to create bottom sheet dialog for adding medication

--- app/src/main/java/com/health/medicationtracker/ui/main/MainClickHandlers.java
+++ app/src/main/java/com/health/medicationtracker/ui/main/MainClickHandlers.java
@@ -1,19 +1,36 @@
 package com.health.medicationtracker.ui.main;
 
 import android.content.Context;
-import android.content.Intent;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.FrameLayout;
+
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
+import com.health.medicationtracker.databinding.AddMedicationBinding;
 
 public class MainClickHandlers {
-    private Context context;
-
+    private final Context context;
+    AddMedicationBinding addMedicationBinding;
     public MainClickHandlers(Context context) {
         this.context = context;
 
     }
 
     public void onAddMedicationClick(View view) {
-        Intent intent = new Intent(context, AddMedication.class);
-        context.startActivity(intent);
+        addMedicationBinding = AddMedicationBinding.inflate(LayoutInflater.from(context));
+        BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(context);
+        bottomSheetDialog.setContentView(addMedicationBinding.getRoot());
+        bottomSheetDialog.show();
+
+        FrameLayout bottomSheet = bottomSheetDialog.findViewById(
+                com.google.android.material.R.id.design_bottom_sheet);
+        if (bottomSheet != null) {
+            BottomSheetBehavior<View> behavior = BottomSheetBehavior.from(bottomSheet);
+            behavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+            behavior.setSkipCollapsed(true);
+        }
+
     }
+
 }
