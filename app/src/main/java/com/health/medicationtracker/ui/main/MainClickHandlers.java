fa138713bcc02816b652171106dd1f1f6d9a02d2
Mandla Mbolekwa <mambolejhb024@student.wethinkcode.co.za>
2025-09-10 20:18:05 +0200

    Update: added medicatonViewModel from source owner, initialized addMedicationHandler in dialog sheet

--- app/src/main/java/com/health/medicationtracker/ui/main/MainClickHandlers.java
+++ app/src/main/java/com/health/medicationtracker/ui/main/MainClickHandlers.java
@@ -4,25 +4,49 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.Toast;
 
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelStoreOwner;
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.health.medicationtracker.databinding.AddMedicationBinding;
+import com.health.medicationtracker.model.Medication;
+import com.health.medicationtracker.viewmodel.MedicationViewModel;
 
 public class MainClickHandlers {
     private final Context context;
     AddMedicationBinding addMedicationBinding;
-    public MainClickHandlers(Context context) {
+    Medication medication;
+    public MainClickHandlers(Context context, Medication medication) {
         this.context = context;
+        this.medication = medication;
 
     }
 
     public void onAddMedicationClick(View view) {
         addMedicationBinding = AddMedicationBinding.inflate(LayoutInflater.from(context));
+
+        // get view model from the class that owns it
+        MedicationViewModel medicationViewModel = new ViewModelProvider((ViewModelStoreOwner) context).get(MedicationViewModel.class);
+
+        // show bottom sheet
         BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(context);
         bottomSheetDialog.setContentView(addMedicationBinding.getRoot());
         bottomSheetDialog.show();
 
+        // create the handler
+        AddMedicationHandler addMedicationHandler = new AddMedicationHandler(
+                context,
+                medication,
+                medicationViewModel, bottomSheetDialog
+        );
+
+        // bind handler to layout (this connects `@{medication::addMedication}` and text fields)
+        addMedicationBinding.setHandler(addMedicationHandler);
+        addMedicationBinding.setMedication(medication);
+
+        // expand sheet
         FrameLayout bottomSheet = bottomSheetDialog.findViewById(
                 com.google.android.material.R.id.design_bottom_sheet);
         if (bottomSheet != null) {
@@ -30,7 +54,5 @@ public class MainClickHandlers {
             behavior.setState(BottomSheetBehavior.STATE_EXPANDED);
             behavior.setSkipCollapsed(true);
         }
-
     }
-
 }
